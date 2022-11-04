44ed2188681ec40de7e697ddb62c089b8e489243
afroozkarampishe <afrooz.karampishe@jobtick.com>
2022-11-05 01:19:47 +0330

    changeProductAndClearBtnStyle

--- src/components/products/Form.tsx
+++ src/components/products/Form.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-const Form = ({ searchProducts, getCategories, categories }: any) => {
+const Form = ({
+  searchProducts,
+  getCategories,
+  categories,
+  products,
+  clearProducts
+}: any) => {
   const [text, setText] = useState('');
   const [category, setCategory] = useState('');
   useEffect(() => {
@@ -14,11 +20,11 @@ const Form = ({ searchProducts, getCategories, categories }: any) => {
   }, [category, text]);
 
   return (
-    <form className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-16">
+    <form className="mt-6 mb-14 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-16">
       <input
         type="search"
         value={text}
-        className="g-gray-50 border-2 border-gray-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-full"
+        className="g-gray-50 border-2 border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-full"
         onChange={(e) => setText(e.target.value)}
       />
       <select
@@ -35,6 +41,14 @@ const Form = ({ searchProducts, getCategories, categories }: any) => {
           </option>
         ))}
       </select>
+      {products.length > 0 && (
+        <button
+          className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-600 px-5 py-2 text-base font-medium text-white hover:bg-red-700"
+          onClick={() => clearProducts()}
+        >
+          CLEAR
+        </button>
+      )}
     </form>
   );
 };
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Product from './Product';
-const Products = ({ products }: any) => {
+const Products = ({ products, clearProducts }: any) => {
   return (
-    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-16">
+    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-14">
       {products.map((product: any) => (
-        <Product key={product.id} product={product} />
+        <Product
+          key={product.id}
+          product={product}
+          clearProducts={clearProducts}
+        />
       ))}
     </div>
   );
